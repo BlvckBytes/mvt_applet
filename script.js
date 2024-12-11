e2da7239ef0e471655e45656973237561e6666e5
Philip Hell <blvckbytes@gmail.com>
2024-12-11 06:26:02 +0100

    refactored to employ promises

--- script.js
+++ script.js
@@ -1,27 +1,67 @@
-const onAppletInit = api => {
+const onAppletInit = async (api) => {
+
+  // ================================================================================
+  // Helpers regarding synchronization and temporary label tracking
+  // ================================================================================
+
   let temporaryLabels = [];
   let unhandledAliveCallLabels = [];
   let aliveListenerByLabel = {};
-  const tangentLength = .5;
 
-  const registerTemporaryLabels = (labelString, aliveListener) => {
-    const labels = labelString.split(',');
-
-    for (const label of labels) {
-      temporaryLabels.push(label);
-
-      if (aliveListener) {
-        if (unhandledAliveCallLabels.includes(label))
-          aliveListener(label);
-        else
-          aliveListenerByLabel[label] = aliveListener;
+  const evaluateCommand = (command, secondaryAliveListener, isPermanent) => {
+    return new Promise((resolve, reject) => {
+      // Need to be able to tell apart main- from secondary labels
+      if (command.includes('\n')) {
+        reject(`Encountered illegal command-count greater than one in command-string "${command}"`);
+        return;
       }
-    }
 
-    return labels[0]
+      const evaluationResult = api.evalCommandGetLabels(command);
+
+      if (typeof evaluationResult !== "string") {
+        reject(`Failed at executing command "${command}"`);
+        return;
+      }
+
+      const createdLabels = evaluationResult.split(',');
+
+      // There's no reason to use this helper for non-creational commands
+      if (createdLabels.length == 0) {
+        reject(`Expected the command "${command} to create at least one object"`);
+        return;
+      }
+
+      const mainLabel = createdLabels[0];
+
+      if (isPermanent !== true)
+        temporaryLabels.push(mainLabel);
+
+      if (typeof secondaryAliveListener === 'function') {
+        for (let i = 1; i < mainLabel.length; ++i) {
+          const secondaryLabel = createdLabels[i];
+
+          if (isPermanent !== true)
+            temporaryLabels.push(secondaryLabel);
+
+          if (unhandledAliveCallLabels.includes(secondaryLabel)) {
+            secondaryAliveListener(secondaryLabel);
+            continue;
+          }
+
+          aliveListenerByLabel[secondaryLabel] = secondaryAliveListener;
+        }
+      }
+
+      if (unhandledAliveCallLabels.includes(mainLabel)) {
+        resolve(mainLabel);
+        return;
+      }
+
+      aliveListenerByLabel[mainLabel] = () => resolve(mainLabel);
+    });
   };
 
-  const clearDivisions = (numberOfDivisions) => {
+  const deleteTemporaryObjects = () => {
     aliveListenerByLabel = {};
 
     for (let i = temporaryLabels.length - 1; i >= 0; --i)
@@ -31,24 +71,37 @@ const onAppletInit = api => {
     temporaryLabels = [];
   };
 
+  api.registerAddListener(addedLabel => {
+    const listener = aliveListenerByLabel[addedLabel];
+
+    if (listener && typeof listener == 'function') {
+      delete aliveListenerByLabel[addedLabel];
+      listener(addedLabel);
+      return;
+    }
+
+    unhandledAliveCallLabels.push(addedLabel);
+  });
+
+  // ================================================================================
+  // Scene definition
+  // ================================================================================
+
   const solveDerivativeAbscissaAndMakePoint = (pointLabel, slopeValueLabel, minXValueLabel, maxXValueLabel) => {
-    return registerTemporaryLabels(
-      api.evalCommandGetLabels(
-        `${pointLabel} = Point({Element(` +
-          'KeepIf(' +
-            `x >= x(${minXValueLabel}) && x <= x(${maxXValueLabel}),`+
-            `NSolutions(f' = ${slopeValueLabel})` +
-          ')' +
-        ', 1), 0})'
-      )
+    return evaluateCommand(
+      `${pointLabel} = Point({Element(` +
+        'KeepIf(' +
+          `x >= x(${minXValueLabel}) && x <= x(${maxXValueLabel}),`+
+          `NSolutions(f' = ${slopeValueLabel})` +
+        ')' +
+      ', 1), 0})'
     );
   };
 
-  const makeTangentSegment = (labelNamePart, abscissaPointLabel, slopeLabel, aliveListener) => {
-    const tangentFunctionLabel = registerTemporaryLabels(
-      api.evalCommandGetLabels(`t_{${labelNamePart}}(x) = ${slopeLabel} * (x - x(${abscissaPointLabel})) + f(x(${abscissaPointLabel}))`),
-      label => api.setVisible(label, false)
-    );
+  const makeTangentSegment = async (labelNamePart, abscissaPointLabel, slopeLabel, pointAndSegmentLabelCallback) => {
+    const tangentFunctionLabel = await evaluateCommand(`t_{${labelNamePart}}(x) = ${slopeLabel} * (x - x(${abscissaPointLabel})) + f(x(${abscissaPointLabel}))`);
+
+    api.setVisible(tangentFunctionLabel, false);
 
     /*
       Keep the tangent line length constant, no matter it's slope.
@@ -66,89 +119,73 @@ const onAppletInit = api => {
       l^2/(4*k^2 + 4)       = a^2
       sqrt(l^2/(4*k^2 + 4)) = a
     */
-    const segmentDeltaXLabel = registerTemporaryLabels(
-      api.evalCommandGetLabels(`a_{${labelNamePart}} = sqrt(${tangentLength}^2 / (4*${slopeLabel}^2 + 4))`)
-    );
+    const tangentLength = .5;
+    const segmentDeltaXLabel = await evaluateCommand(`a_{${labelNamePart}} = sqrt(${tangentLength}^2 / (4*${slopeLabel}^2 + 4))`);
 
     const sX = `x(${abscissaPointLabel}) - ${segmentDeltaXLabel}`;
     const eX = `x(${abscissaPointLabel}) + ${segmentDeltaXLabel}`;
 
     // I've tried to simply plot the function t(x) in [sX;eX], but got horrible lag - thus, let's instantiate a segment manually
-    registerTemporaryLabels(
-      api.evalCommandGetLabels(`t_s_{${labelNamePart}} = Segment((${sX}, ${tangentFunctionLabel}(${sX})), (${eX}, ${tangentFunctionLabel}(${eX})))`),
-      label => {
-        if (aliveListener)
-          aliveListener(label)
+    const segmentLabel = await evaluateCommand(`t_s_{${labelNamePart}} = Segment((${sX}, ${tangentFunctionLabel}(${sX})), (${eX}, ${tangentFunctionLabel}(${eX})))`);
 
-        api.setLabelVisible(label, false);
-      }
-    );
+    api.setLabelVisible(segmentLabel, false);
 
-    registerTemporaryLabels(
-      api.evalCommandGetLabels(`T_{${labelNamePart}} = Point({x(${abscissaPointLabel}), f(x(${abscissaPointLabel}))})`),
-      label => {
-        if (aliveListener)
-          aliveListener(label)
+    if (pointAndSegmentLabelCallback)
+      pointAndSegmentLabelCallback(segmentLabel);
 
-        api.setLabelVisible(label, false);
-      }
-    );
+    const pointLabel = await evaluateCommand(`T_{${labelNamePart}} = Point({x(${abscissaPointLabel}), f(x(${abscissaPointLabel}))})`);
+
+    api.setLabelVisible(pointLabel, false);
+
+    if (pointAndSegmentLabelCallback)
+      pointAndSegmentLabelCallback(pointLabel);
   };
 
-  const setupDivision = (divisionIndex, previousPointLabel, currentPointLabel) => {
-    registerTemporaryLabels(
-      api.evalCommandGetLabels(`S_{D${divisionIndex}} = Segment(${previousPointLabel}, ${currentPointLabel})`),
-      label => {
-        api.setLabelVisible(label, false),
-        api.setColor(label, 0, 0, 0);
-      }
-    );
+  const setupDivisionAndGetSecantSlopeLabel = async (divisionIndex, previousPointLabel, currentPointLabel) => {
+    const divisionSecantLabel = await evaluateCommand(`S_{D${divisionIndex}} = Segment(${previousPointLabel}, ${currentPointLabel})`);
 
-    const secantSlopeLabel = registerTemporaryLabels(
-      api.evalCommandGetLabels(`s_{D${divisionIndex}} = (y(${previousPointLabel}) - y(${currentPointLabel})) / (x(${previousPointLabel}) - x(${currentPointLabel}))`),
-    );
+    api.setLabelVisible(divisionSecantLabel, false),
+    api.setColor(divisionSecantLabel, 0, 0, 0);
 
-    const abscissaPointLabel = solveDerivativeAbscissaAndMakePoint(`μ_{${divisionIndex}}`, secantSlopeLabel, previousPointLabel, currentPointLabel);
+    const secantSlopeLabel = await evaluateCommand(`s_{D${divisionIndex}} = (y(${previousPointLabel}) - y(${currentPointLabel})) / (x(${previousPointLabel}) - x(${currentPointLabel}))`);
 
-    makeTangentSegment(
+    const abscissaPointLabel = await solveDerivativeAbscissaAndMakePoint(`μ_{${divisionIndex}}`, secantSlopeLabel, previousPointLabel, currentPointLabel);
+
+    await makeTangentSegment(
       `D${divisionIndex}`, abscissaPointLabel, secantSlopeLabel,
       label => api.setColor(label, 255, 0, 255)
     );
 
-    const fPrimePointLabel = registerTemporaryLabels(
-      api.evalCommandGetLabels(`F_{μ${divisionIndex}} = (x(${abscissaPointLabel}), f'(x(${abscissaPointLabel})))`),
-      label => api.setLabelVisible(label, false)
-    );
+    const fPrimePointLabel = await evaluateCommand(`F_{μ${divisionIndex}} = (x(${abscissaPointLabel}), f'(x(${abscissaPointLabel})))`);
 
-    registerTemporaryLabels(
-      api.evalCommandGetLabels(`V_{μ${divisionIndex}} = Segment(${fPrimePointLabel}, ${abscissaPointLabel})`),
-      label => api.setLabelVisible(label, false)
-    );
+    api.setLabelVisible(fPrimePointLabel, false)
+
+    const fPrimeLineLabel = await evaluateCommand(`V_{μ${divisionIndex}} = Segment(${fPrimePointLabel}, ${abscissaPointLabel})`);
+
+    api.setLabelVisible(fPrimeLineLabel, false)
+
+    return secantSlopeLabel
   };
 
-  const setupDivisions = (numberOfDivisions) => {
+  const setupDivisions = async (numberOfDivisions) => {
+    const secantSlopeLabels = [];
+
     let previousPointLabel = null;
     let firstPointLabel = null;
     let abscissaPointLabel = null;
 
     // One more division, as the end of the last is the beginning of n+1
     for (let i = 1; i <= numberOfDivisions + 1; ++i) {
-      const abscissaLabel = registerTemporaryLabels(
-        api.evalCommandGetLabels(`x_{D${i}} = x_{AB}(${i})`)
-      );
+      const abscissaLabel = await evaluateCommand(`x_{D${i}} = x_{AB}(${i})`);
 
-      const pointLabel = registerTemporaryLabels(
-        api.evalCommandGetLabels(`F_{D${i}} = (${abscissaLabel}, f(${abscissaLabel}))`),
-        label => api.setLabelVisible(label, false)
-      );
+      const pointLabel = await evaluateCommand(`F_{D${i}} = (${abscissaLabel}, f(${abscissaLabel}))`);
+      api.setLabelVisible(pointLabel, false)
 
-      registerTemporaryLabels(
-        api.evalCommandGetLabels(`V_{D${i}} = Segment((x(${pointLabel}), 0), ${pointLabel})`),
-        label => api.setLabelVisible(label, false)
-      );
+      const lineLabel = await evaluateCommand(`V_{D${i}} = Segment((x(${pointLabel}), 0), ${pointLabel})`);
+      api.setLabelVisible(lineLabel, false)
 
       if (previousPointLabel != null)
-        setupDivision(i - 1, previousPointLabel, pointLabel);
+        secantSlopeLabels.push(await setupDivisionAndGetSecantSlopeLabel(i - 1, previousPointLabel, pointLabel));
 
       previousPointLabel = pointLabel;
 
@@ -156,32 +193,19 @@ const onAppletInit = api => {
         firstPointLabel = pointLabel;
 
       if (numberOfDivisions != 1 && i == numberOfDivisions + 1) {
-        registerTemporaryLabels(
-          api.evalCommandGetLabels(`S_G = Segment(${firstPointLabel}, ${pointLabel})`),
-          label => {
-            api.setColor(label, 255, 0, 0);
-            api.setLabelVisible(label, false);
-          }
-        );
+        const globalSecantLabel = await evaluateCommand(`S_G = Segment(${firstPointLabel}, ${pointLabel})`);
 
-        // secant slope labels are of pattern s_{D${divisionIndex}}
-        // let s_l be their level magnitude
+        api.setColor(globalSecantLabel, 255, 0, 0);
+        api.setLabelVisible(globalSecantLabel, false);
 
-        const slopeValueLabels = [];
+        const slopeLevelTermLabel = await evaluateCommand(`s_G = (${secantSlopeLabels.join("+")})/${numberOfDivisions}`);
 
-        for (let divisionIndex = 1; divisionIndex <= numberOfDivisions; ++divisionIndex)
-          slopeValueLabels.push(`s_{D${divisionIndex}}`);
-
-        const slopeLevelTermLabel = registerTemporaryLabels(
-          api.evalCommandGetLabels(`s_G = (${slopeValueLabels.join("+")})/${numberOfDivisions}`)
-        );
-
-        abscissaPointLabel = solveDerivativeAbscissaAndMakePoint(
+        abscissaPointLabel = await solveDerivativeAbscissaAndMakePoint(
           "μ", slopeLevelTermLabel, "A", "B",
           label => api.setColor(label, 128, 0, 255)
         );
 
-        makeTangentSegment(
+        await makeTangentSegment(
           `G`, abscissaPointLabel, slopeLevelTermLabel,
           label => api.setColor(label, 128, 0, 255)
         );
@@ -191,118 +215,71 @@ const onAppletInit = api => {
         abscissaPointLabel = "μ_1";
     }
 
-    const derivativePointLabel = registerTemporaryLabels(
-      api.evalCommandGetLabels(`L_{f'} = Point({x(${abscissaPointLabel}), f'(x(${abscissaPointLabel}))})`),
-      label => {
-        api.setColor(label, 128, 0, 255);
-        api.setLabelVisible(label, false);
-      }
+    const derivativePointLabel = await evaluateCommand(`L_{f'} = Point({x(${abscissaPointLabel}), f'(x(${abscissaPointLabel}))})`);
+
+    api.setColor(derivativePointLabel, 128, 0, 255);
+    api.setLabelVisible(derivativePointLabel, false);
+
+    const derivativeLineLabel = await evaluateCommand(`V_{f'} = Segment(${derivativePointLabel}, ${abscissaPointLabel})`);
+
+    api.setColor(derivativeLineLabel, 128, 0, 255);
+    api.setLabelVisible(derivativeLineLabel, false);
+
+    const polygonPointAPrimeLabel = await evaluateCommand(`Q_{A'} = Point({x(A), y(${derivativePointLabel})})`);
+    api.setVisible(polygonPointAPrimeLabel, false);
+
+    const polygonPointBPrimeLabel = await evaluateCommand(`Q_{B'} = Point({x(B), y(${derivativePointLabel})})`);
+    api.setVisible(polygonPointBPrimeLabel, false);
+
+    const polygonLabel = await evaluateCommand(
+      `Q_{f'} = Polygon(A, B, ${polygonPointBPrimeLabel}, ${polygonPointAPrimeLabel})`,
+      polygonVertexLabel => api.setVisible(polygonVertexLabel, false)
     );
 
-    registerTemporaryLabels(
-      api.evalCommandGetLabels(`V_{f'} = Segment(L_{f'}, ${abscissaPointLabel})`),
-      label => {
-        api.setColor(label, 128, 0, 255);
-        api.setLabelVisible(label, false);
-      }
-    );
-
-    // TODO: This could most definitely use some async-await de-nesting, ^^"
-
-    registerTemporaryLabels(
-      api.evalCommandGetLabels(`Q_{B'} = Point({x(B), y(${derivativePointLabel})})`),
-      polygonPointBPrimeLabel => {
-        api.setVisible(polygonPointBPrimeLabel, false);
-
-        registerTemporaryLabels(
-          api.evalCommandGetLabels(`Q_{A'} = Point({x(A), y(${derivativePointLabel})})`),
-          polygonPointAPrimeLabel => {
-            api.setVisible(polygonPointAPrimeLabel, false);
-
-            registerTemporaryLabels(
-              api.evalCommandGetLabels(`Q_{f'} = Polygon(A, B, ${polygonPointBPrimeLabel}, ${polygonPointAPrimeLabel})`),
-              polygonMemberLabel => {
-                if (polygonMemberLabel == "Q_{f'}") {
-                  api.setLabelVisible(polygonMemberLabel, false);
-                  api.setColor(polygonMemberLabel, 0, 255, 0);
-                  api.setFilling(polygonMemberLabel, .3);
-                  return;
-                }
-
-                api.setVisible(polygonMemberLabel, false);
-              }
-            )
-          }
-        );
-      }
-    );
+    api.setLabelVisible(polygonLabel, false);
+    api.setColor(polygonLabel, 0, 255, 0);
+    api.setFilling(polygonLabel, .3);
   };
-
-  const handleAliveListener = (name) => {
-    const listener = aliveListenerByLabel[name];
-
-    if (listener && typeof listener == 'function') {
-      delete aliveListenerByLabel[name];
-      listener(name);
-      return;
-    }
-
-
-    unhandledAliveCallLabels.push(name);
-  };
-
-  // Not sure if re-introduced, deleted objects call "Add" again - encountered some odd behavior; better safe than sorry.
-  api.registerAddListener(name => handleAliveListener(name));
-  api.registerUpdateListener(name => handleAliveListener(name));
 
   // Number of equally sized divisions between A and B
-  const sliderLabel = api.evalCommandGetLabels("k = Slider(1, 5, 1)");
-  let previousSliderValue = api.getValue(sliderLabel);
+  const sliderLabel = await evaluateCommand("k = Slider(1, 5, 1)", null, true);
 
-  // Rebuild divisions if the number of divisions has changed
   api.registerObjectUpdateListener(sliderLabel, () => {
-    const sliderValue = api.getValue(sliderLabel);
-    clearDivisions(previousSliderValue);
-    setupDivisions(sliderValue);
-    previousSliderValue = sliderValue;
+    deleteTemporaryObjects();
+    setupDivisions(api.getValue(sliderLabel));
   });
 
-  api.evalCommandGetLabels("f(x) = 1/4 * x^3 + 1");
+  const fLabel = await evaluateCommand("f(x) = 1/4 * x^3 + 1", null, true);
 
-  // Rebuild divisions if the input-box successfully parsed a new expression for f
-  api.registerObjectUpdateListener("f", () => {
-    const isValid = api.isDefined("f");
+  api.registerObjectUpdateListener(fLabel, () => {
+    deleteTemporaryObjects();
 
-    clearDivisions(sliderValue);
-
-    if (isValid)
-      setupDivisions(sliderValue);
+    // Rebuild divisions only if the input-box successfully parsed a new expression for f
+    if (api.isDefined(fLabel))
+      setupDivisions(api.getValue(sliderLabel));
   });
 
-  api.setCaption(
-    api.evalCommandGetLabels("InputBox(f)"),
-    "f(x)"
-  );
+  const inputBoxLabel = await evaluateCommand(`InputBox(${fLabel})`, null, true);
+  api.setCaption(inputBoxLabel, "f(x)");
 
-  api.evalCommand("f'(x) = Derivative(f)");
+  const fPrimeLabel = await evaluateCommand(`f'(x) = Derivative(${fLabel})`, null, true);
 
   // Constrain points to coincide with the x-axis (y=0, x=variable)
-  api.evalCommand("a = -1");
-  api.evalCommand("b = 1");
-  api.evalCommand("A = (a, y(yAxis))");
-  api.evalCommand("B = (b, y(yAxis))");
+  await evaluateCommand("a = -1", null, true);
+  await evaluateCommand("b = 1", null, true);
+  await evaluateCommand(`A = (a, y(yAxis))`, null, true);
+  await evaluateCommand(`B = (b, y(yAxis))`, null, true);
 
-  const derivativeAreaLabel = api.evalCommandGetLabels("A_{f'} = Integral(f', x(A), x(B))");
+  const derivativeAreaLabel = await evaluateCommand(`A_{f'} = Integral(${fPrimeLabel}, x(A), x(B))`, null, true);
+
   api.setLabelVisible(derivativeAreaLabel, false);
   api.setColor(derivativeAreaLabel, 255, 0, 0);
   api.setFilling(derivativeAreaLabel, .3);
 
-  // Beginning abscissa of the i-th's (1-based) division
-  api.setVisible(
-    api.evalCommandGetLabels(`x_{AB}(i) = x(A) + (x(B) - x(A))/${sliderLabel} * (i-1)`),
-    false
-  );
+  const beginningAbscissaLabel = await evaluateCommand(`x_{AB}(i) = x(A) + (x(B) - x(A))/${sliderLabel} * (i-1)`, null, true);
+
+  api.setVisible(beginningAbscissaLabel, false);
 
   // Setup based on the initial slider's value
-  setupDivisions(previousSliderValue);
+  setupDivisions(api.getValue(sliderLabel));
 }
